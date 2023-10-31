d09c8bb7dcdf88a6d4ad5af93e00cfb6284cb10a
FPanos <fpanos@triarail.com>
2023-10-31 16:27:46 +0100

    six

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,8 +4,14 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import React, { useState } from 'react';
+
+function createPageURL (page:number){
+  return `/${page}`
+}
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  const [currentPage, setCurrentPage] = useState(0)
   const allPages = generatePagination(currentPage, totalPages);
 
   return (
